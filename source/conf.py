de4e1d6816e71e45b89bde258becd2fe81394709
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2017-07-30 00:59:36 +0100

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -129,7 +129,7 @@ html_theme_options = {
     # * superhero
     # * united
     # * yeti (v3 only)
-    'bootswatch_theme': "cyborg",
+    'bootswatch_theme': "simplex",
 
     # Choose Bootstrap version.
     # Values: "3" (default) or "2" (in quotes)

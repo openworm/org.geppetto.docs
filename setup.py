67f2edc4e02d18e7310ae9509226ae4e94a5ef15
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2019-05-13 15:36:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='geppettodocs',
-    version="0.4.2",
+    version="1.0.0",
     license="MIT",
     description='Geppetto documentation',
     author='Geppetto contributors',
